61f806fa78710ef09ba7f2e41efaa2cc856aedca
minhfli <vuvanminh.bacninh@gmail.com>
2024-08-09 13:49:04 +0700

    fix: text predict

--- src/pages/project/build/steps/step_four.jsx
+++ src/pages/project/build/steps/step_four.jsx
@@ -41,7 +41,10 @@ const StepFour = (props) => {
 		initialState
 	)
 	const [explainImageUrl, setExplainImageUrl] = useState('')
+	const [sentence, setSentence] = useState('')
+	const [explainTextHTML, setExplainTextHTML] = useState('')
 
+	const [predictTextLabel, setPredictTextLabel] = useState('')
 	const [GraphJSON, setGraphJSON] = useState({})
 	const [trainlossGraph, setTrainlossGraph] = useState([])
 	const [val_lossGraph, setVallossGraph] = useState([])
@@ -65,6 +68,10 @@ const StepFour = (props) => {
 			})
 	}, [])
 
+
+	const handleTextChange = (event) => {
+		setSentence(event.target.value)
+	}
 	const readChart = (contents, setGraph) => {
 		const lines = contents.split('\n')
 		const header = lines[0].split(',')
@@ -180,6 +187,115 @@ const StepFour = (props) => {
 	//         console.error(error);
 	//     }
 	// };
+
+	const handlePredictText = async (event) => {
+		event.preventDefault()
+
+		// TODO: fix hardcorded values
+		const formData = new FormData()
+
+		const model = await instance.get(API_URL.get_model(experimentName))
+		const jsonObject = model.data
+		if (!jsonObject) {
+			console.error('Failed to get model info')
+		}
+		console.log(jsonObject)
+
+		formData.append('userEmail', jsonObject.userEmail)
+		formData.append('projectName', jsonObject.projectName)
+		formData.append('runName', 'ISE')
+		formData.append('text_col', "sentence")
+		formData.append('text', sentence)
+
+		console.log('Fetching predict text')
+
+		const url = `${process.env.REACT_APP_EXPLAIN_URL}/text_prediction/temp_predict`
+
+		const options = {
+			method: 'POST',
+			body: formData,
+		}
+
+		fetchWithTimeout(url, options, 60000)
+			.then((data) => {
+				setPredictTextLabel(data.predictions)
+				console.log('Fetch successful')
+			})
+			.catch((error) => {
+				console.error('Fetch error:', error.message)
+				// Handle timeout or other errors here
+				if (error.message === 'Request timed out') {
+					console.log('The request took too long and was terminated.')
+				}
+			}).finally(() => {
+				updateState({ isLoading: false })
+			}
+	}
+
+	const handleExplainText = async (event) => {
+		event.preventDefault()
+
+		// TODO: fix hardcorded values
+		const formData = new FormData()
+
+		updateState({
+			isLoading: true,
+		})
+
+		const model = await instance.get(API_URL.get_model(experimentName))
+		const jsonObject = model.data
+		if (!jsonObject) {
+			console.error('Failed to get model info')
+		}
+		console.log(jsonObject)
+
+		formData.append('userEmail', jsonObject.userEmail)
+		formData.append('projectName', jsonObject.projectName)
+		formData.append('runName', 'ISE')
+		formData.append('text', sentence)
+
+		console.log('Fetching explain text')
+
+		const url = `${process.env.REACT_APP_EXPLAIN_URL}/text_prediction/explain`
+
+		const options = {
+			method: 'POST',
+			body: formData,
+		}
+
+		fetchWithTimeout(url, options, 60000)
+			.then((data) => {
+				const html = data.explain_html
+				setExplainTextHTML(html)
+				console.log(html)
+				const parsedHTML = new DOMParser().parseFromString(
+					html,
+					'text/html'
+				)
+				const scriptContent =
+					parsedHTML.querySelector('script').textContent
+				// Create a script element
+				const script = document.createElement('script')
+
+				// Set the script content to execute
+				script.textContent = scriptContent
+
+				// Append the script element to the document body or head
+				// You can choose where to append it based on your needs
+				document.body.appendChild(script)
+
+				console.log('Fetch successful')
+			})
+			.catch((error) => {
+				console.error('Fetch error:', error.message)
+				// Handle timeout or other errors here
+				if (error.message === 'Request timed out') {
+					console.log('The request took too long and was terminated.')
+				}
+			}).finally(() => {
+				updateState({ isLoading: false })
+			}
+	}
 
 	const handleExplainSelectedImage = async () => {
 		const item = stepFourState.selectedImage
@@ -530,7 +646,45 @@ const StepFour = (props) => {
 					</div>
 				</Dialog>
 			</Transition.Root>
-			<div></div>
+			<div
+				className={`${
+					stepFourState.showTextModal
+						? 'top-0 left-0 bottom-full z-[1000] opacity-100'
+						: 'left-0 top-full bottom-0 opacity-0'
+				} fixed flex flex-col items-center h-full w-full px-[30px] justify-center bg-white  transition-all duration-500 ease overflow-auto`}
+			>
+				<h1>Text</h1>
+				<form onSubmit={handleExplainText}>
+					<input
+						style={{
+							border: '2px solid #ccc',
+							borderRadius: '4px',
+							padding: '10px',
+							width: '100%',
+							boxSizing: 'border-box',
+						}}
+						type="text"
+						value={sentence}
+						name="sentence"
+						onChange={handleTextChange}
+					/>
+					<button type="submit">Submit</button>
+				</form>
+				<div>
+					{predictTextLabel && <p>{predictTextLabel}</p>}
+				</div>
+				<div
+					style={{
+						width: '100%',
+						maxWidth: '1000px',
+						padding: '20px',
+						backgroundColor: '#f9f9f9',
+						borderRadius: '8px',
+						overflow: 'auto',
+					}}
+					dangerouslySetInnerHTML={{ __html: explainTextHTML }}
+				></div>
+			</div>
 			<div
 				className={`${
 					stepFourState.showUploadModal
