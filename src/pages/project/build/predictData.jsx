a004d21cd2aec50d88e28d28e9aabc49e1401952
seg_fault1611 <darklord1611@gmail.com>
2024-11-17 14:59:44 +0000

    fix: quick fix merge error

--- src/pages/project/build/predictData.jsx
+++ src/pages/project/build/predictData.jsx
@@ -27,7 +27,6 @@ const initialState = {
 	uploadSentences: [],
 	predictResult: {},
 	predictEndpoint: null,
-	predictEndpoint: null,
 }
 
 const PredictData = (props) => {
@@ -128,7 +127,6 @@ const PredictData = (props) => {
 		console.log('Fetch start')
 
 		// TODO: change here
-		// TODO: change here
 		try {
 			const { data } = await experimentAPI.predictData(
 				experimentName,
@@ -190,7 +188,7 @@ const PredictData = (props) => {
 			updateProjState({
 				predictEndpoint: data.url,
 			})
-		} catch(error) {
+		} catch (error) {
 			console.log('Deploy model failed', error)
 		} finally {
 			updateProjState({
@@ -247,10 +245,6 @@ const PredictData = (props) => {
 
 			{/* UPLOAD VIEW */}
 			<div
-				className={`${predictDataState.showUploadPanel
-					? 'top-[375px] left-0 bottom-full z-[1000] opacity-100'
-					: 'left-0 top-full bottom-0 opacity-0'
-					} fixed h-full w-full bg-white transition-all duration-500 ease overflow-auto pb-[30px]`}
 				className={`${predictDataState.showUploadPanel
 					? 'top-[375px] left-0 bottom-full z-[1000] opacity-100'
 					: 'left-0 top-full bottom-0 opacity-0'
@@ -326,7 +320,6 @@ const PredictData = (props) => {
 
 			{/* PREDICT VIEW */}
 			{predictDataState.uploadedFiles.length > 0 &&
-				predictDataState.showPredictLayout ? (
 				predictDataState.showPredictLayout ? (
 				projectInfo &&
 				(() => {
