3e8b751fc63a0fc7e1cf5e2a5c513b57086bf459
galaxyImpact204 <tranduytuana123@gmail.com>
2024-11-26 23:06:00 +0700

    :sparkles:feat: Select Instance before train

--- src/pages/project/build/Dashboard.jsx
+++ src/pages/project/build/Dashboard.jsx
@@ -242,31 +242,11 @@ const Dashboard = ({ updateFields, projectInfo }) => {
 		}
 	}
 
-	const trainModel = async () => {
-		if (projectID && selectedDataset !== null) {
-			const dataset = filteredDatasets[selectedDataset]
-			updateProjState({ isUploading: true })
-			try {
-				const res = await projectAPI.trainModel(projectID, dataset)
-
-				searchParams.set('experiment_name', res.data.data.task_id)
-				setSearchParams(searchParams)
-
-				updateFields({
-					isDoneUploadData: true,
-					instanceInfor: res.data.instance_info,
-				})
-
-				message.success('Successfully Choose Dataset', 3)
-			} catch (error) {
-				console.error('Error training model:', error)
-				message.error('Choose Dataset Failed', 3)
-			} finally {
-				updateProjState({ isUploading: false })
-			}
-		} else {
-			console.log('No dataset or missing ProjectID')
-		}
+	const selectInstance = () => {
+		updateFields({
+			isDoneUploadData: true,
+			selectedDataset: filteredDatasets[selectedDataset],
+		})
 	}
 
 	const bucketList = ['user-private-dataset', 'bucket-1']
@@ -308,7 +288,7 @@ const Dashboard = ({ updateFields, projectInfo }) => {
 					{/* BUTTON */}
 					<div className="w-full pt-10 flex justify-center items-center">
 						{selectedDataset !== null && (
-							<button className="btn" onClick={trainModel}>
+							<button className="btn" onClick={selectInstance}>
 								<svg
 									height="24"
 									width="24"
@@ -320,7 +300,7 @@ const Dashboard = ({ updateFields, projectInfo }) => {
 								>
 									<path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
 								</svg>
-								<span className="text">Launch Instance</span>
+								<span className="text">Next</span>
 							</button>
 						)}
 					</div>
