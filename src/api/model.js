ebe2629d501642e114e28bebcbaeee9624195e9e
thanhvinh0702 <thanhvinhhero0702@gmail.com>
2025-07-07 03:33:00 +0700

    fit backend api for validation graph

---
+++ src/api/model.js
@@ -0,0 +1,8 @@
+import { API_BASE_URL, API_URL } from "src/constants/api";
+import instance from "./axios";
+
+const URL = `${API_BASE_URL}/api/ml/database_service/models_service`
+
+const getModels = () => {
+
+}
