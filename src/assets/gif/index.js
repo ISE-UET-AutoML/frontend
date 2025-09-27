21ab97bf2608a987ac565c2e170d2acf408f5e47
viet2005-68 <23020170@vnu.edu.vn>
2025-09-27 20:19:16 +0000

    add gif img

---
+++ src/assets/gif/index.js
@@ -0,0 +1,42 @@
+import artificialIntelligence from './artificial-intelligence.gif';
+import cloud from './cloud.gif';
+import software from './software.gif';
+import file from './file.gif';
+import chatLoading from './chat_loading.svg';
+import Aipoweredmarketingtoolsabstract from './Ai-powered-marketing-tools-abstract.gif';
+import AITools from './AI-Tools.gif';
+import KeitoFrontPage from './Keito-FrontPage-Animation.gif';
+import LiveChatbot from './Live-chatbot.gif';
+import SandyLoading from './Sandy-Loading.gif';
+import ChatGif1 from './chatgif1.gif';
+import LiquidLoading from './liquid.gif';
+export {
+    artificialIntelligence,
+    cloud,
+    software,
+    file,
+    chatLoading,
+    Aipoweredmarketingtoolsabstract,
+    AITools,
+    KeitoFrontPage,
+    LiveChatbot,
+    SandyLoading,
+    ChatGif1,
+    LiquidLoading
+};
+
+export default {
+    artificialIntelligence,
+    cloud,
+    software,
+    file,
+    chatLoading,
+    Aipoweredmarketingtoolsabstract,
+    AITools,
+    KeitoFrontPage,
+    LiveChatbot,
+    SandyLoading,
+    ChatGif1,
+    LiquidLoading
+};
+
--- src/pages/project/build/Loading.jsx
+++ src/pages/project/build/Loading.jsx
@@ -1,36 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 import {
-  artificialIntelligence,
-  cloud,
-  software,
-  file,
-  chatLoading,
-  Aipoweredmarketingtoolsabstract,
-  AITools,
-  KeitoFrontPage,
-  LiveChatbot,
-  SandyLoading,
-} from "src/assets/gif";
+	artificialIntelligence,
+	Aipoweredmarketingtoolsabstract,
+	LiveChatbot,
+	SandyLoading,
+	ChatGif1,
+	LiquidLoading,
+} from 'src/assets/gif'
 
-const gifs = [
-  software,
-  cloud,
-  file,
-  Aipoweredmarketingtoolsabstract,
-  LiveChatbot
-];
+const gifs = [LiquidLoading, LiquidLoading, ChatGif1]
 
 export default function Loading({ currentStep }) {
-  return (
-    <div className="flex flex-col items-center justify-center py-12 gap-4" style={{marginTop: '50px'}}>
-      <img
-        src={gifs[currentStep]}
-        alt="loading"
-        style={{ width: "300px", height: "300px" }}
-      />
-      <p style={{ color: "var(--text)", fontSize: "18px", marginTop: "20px" }}>
-        It may take a while, you can exit and come back later.
-      </p>
-    </div>
-  );
+	return (
+		<div
+			className="flex flex-col items-center justify-center py-12 gap-4"
+			style={{ marginTop: '50px' }}
+		>
+			<img
+				src={gifs[currentStep]}
+				alt="loading"
+				style={{ width: '700px', height: '500px' }}
+			/>
+			<p
+				style={{
+					color: 'var(--text)',
+					fontSize: '18px',
+					marginTop: '20px',
+				}}
+			>
+				It may take a while, you can exit and come back later.
+			</p>
+		</div>
+	)
 }
