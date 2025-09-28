b4933d21b5a23f6d8280b04e920845e92b1988fa
viet2005-68 <23020170@vnu.edu.vn>
2025-09-28 03:02:16 +0000

    add iframe for training progress

--- src/pages/project/build/Loading.jsx
+++ src/pages/project/build/Loading.jsx
@@ -8,19 +8,39 @@ import {
 	LiquidLoading,
 } from 'src/assets/gif'
 
-const gifs = [LiquidLoading, LiquidLoading, ChatGif1]
-
 export default function Loading({ currentStep }) {
+	const iframes = [
+		'https://lottie.host/embed/1d947a49-e3c3-4829-b6a3-4aad8b315a3e/2be5iOiE2i.lottie',
+		'https://lottie.host/embed/45dede61-c804-4fba-9143-ad8ac39c0cb2/Ma1FXatlzi.lottie',
+		'https://lottie.host/embed/f6ec3dec-0fca-412a-8538-cea8aeca6c5c/gPWIC8O3wf.lottie',
+	]
+
+	const [index, setIndex] = useState(0)
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((prev) => (prev + 1) % iframes.length)
+		}, 10000)
+
+		return () => clearInterval(interval)
+	}, [iframes.length])
 	return (
 		<div
 			className="flex flex-col items-center justify-center py-12 gap-4"
 			style={{ marginTop: '50px' }}
 		>
-			<img
-				src={gifs[currentStep]}
-				alt="loading"
-				style={{ width: '700px', height: '500px' }}
-			/>
+			<div className="relative w-full h-[400px] overflow-hidden">
+				{iframes.map((src, i) => (
+					<iframe
+						key={i}
+						src={src}
+						className={`absolute top-0 left-0 w-full h-full border-0 transition-opacity duration-1000 ${
+							i === index ? 'opacity-100' : 'opacity-0'
+						}`}
+					></iframe>
+				))}
+			</div>
+
 			<p
 				style={{
 					color: 'var(--text)',
