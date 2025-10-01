7fe531face5be4599cb25d4be8111329dc1d7018
giap <danggiaprika@gmail.com>
2025-10-01 17:37:26 +0000

    update polling dataset

--- src/components/projects/WaitForPollingSuccess.jsx
+++ src/components/projects/WaitForPollingSuccess.jsx
@@ -1,30 +1,18 @@
 import * as datasetAPI from 'src/api/dataset';
 
-export const WaitForPollingSuccess = (datasetId, timeout = 1000000, interval = 2000) => {
-    return new Promise((resolve, reject) => {
-        const start = Date.now();
+export const WaitForPollingSuccess = async (datasetId, interval = 5000) => {
+    while (true) {
+        try {
+            const res = await datasetAPI.getDataset(datasetId);
+            const dataset = res.data;
 
-        const check = async () => {
-            try {
-                const res = await datasetAPI.getDataset(datasetId);
-                const dataset = res.data;
-
-                if (dataset.ls_project && dataset.ls_project.label_studio_id) {
-                    resolve(dataset);
-                    return;
-                }
-
-                if (Date.now() - start > timeout) {
-                    reject(new Error("Polling timeout"));
-                    return;
-                }
-
-                setTimeout(check, interval);
-            } catch (err) {
-                reject(err);
+            if (dataset.ls_project && dataset.ls_project.label_studio_id) {
+                return dataset;
             }
-        };
+        } catch (err) {
+            throw err; 
+        }
 
-        check();
-    });
+        await new Promise(resolve => setTimeout(resolve, interval));
+    }
 };
